abda37a84beb15dd2f593c08227b27b6ef1718c1
Mbocharov <msbocharov@gmail.com>
2018-11-11 10:25:18 +0200

    example of tdd approach

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ JS TDD Workshop
 ===============
 
 Base repository for [Test Driven Development](https://en.wikipedia.org/wiki/Test-driven_development) workshop with [JavaScript](https://www.javascript.com/)
+
+Task:
+Given a non-empty array of unique integers, return the k maximum numbers from the array in any order without using a built in functions.
+
+For example the array is [1, 3, 8, 4, 2, 6, 7] and k is 4 then the result should contain [8, 6, 7, 4] 
+
---
+++ test/filter.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-plusplus */
+require('chai').should();
+require('mocha');
+
+
+function filter(items, k) {
+    const max = [];
+    for (let i = 0; i < k; i++) {
+        max[i] = items[i];
+    }
+    for (let i = 1; i < items.length; i++) {
+        let j = 0;
+        while (j < k && !(items[i] <= max[j])) {
+            if (!items[i] > max[j]) {
+                j++;
+            }
+            if (j < k) {
+                max[j] = items[i];
+            }
+        }
+    }
+    return max;
+}
+
+describe('filter', () => {
+    it('has to filter one item from single item array', () => {
+        filter([1], 1).should.eql([1]);
+    });
+
+    it('has to filter one item from two array items when firs is largest', () => {
+        filter([2, 1], 1).should.eql([2]);
+    });
+
+    it('has to filter one item from two items array when last is largest', () => {
+        filter([1, 2], 1).should.eql([2]);
+    });
+
+    it('has to filter one item from 3 items array when last is largest', () => {
+        filter([1, 2, 3], 1).should.eql([3]);
+    });
+
+    it('has to filter two item from 3 items array ', () => {
+        const result = filter([1, 2, 3], 2);
+        result.should.have.members([2, 3]);
+        result.length.should.eql(2);
+    });
+});
